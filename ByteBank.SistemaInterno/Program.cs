a3faf46d7063c7491f18ffccbddb8bf5efc5b514
daniloc <danilo.costa@me.com.br>
2019-04-01 18:35:04 -0300

    Array primeira aula

--- ByteBank.SistemaInterno/Program.cs
+++ ByteBank.SistemaInterno/Program.cs
@@ -11,7 +11,37 @@ namespace ByteBank.SistemaInterno {
     class Program {
         static void Main (string[] args)
         {
-            string textFirst = "Hey, my number is 967023470.";
+            int[] idades = new int[]{
+                1,
+                2,
+                3,
+                4,
+                5
+            };
+
+            int somaIdade = 0;
+
+            for (int indice = 0; indice < idades.Length; indice++)
+            {
+                Console.WriteLine($"Indice na possisão {indice}: idade {idades[indice]} ");
+                somaIdade += idades[indice];
+            }
+            Console.WriteLine($"A medida de idade é: {somaIdade / idades.Length}");
+        }
+
+        private static int[] ArrayDeInt()
+        {
+            int[] idades = new int[5];
+            idades[0] = 1;
+            idades[1] = 2;
+            idades[2] = 3;
+            idades[3] = 4;
+            idades[4] = 5;
+            return idades;
+        }
+
+        private static void ManipulacaoDeString(){
+                        string textFirst = "Hey, my number is 967023470.";
             string textSecond = "Hellow, the number of smartphone 67023470 is her.";
             string textFinally = "96702-3470, It's number of my smartphone.";
             string padrao = "[0-9]{4,5}-?[0-9]{4}";
