f922ae2834191e37a0f90fbc916c0fdf354bf894
daniloc <danilo.costa@me.com.br>
2019-04-09 19:08:55 -0300

    Final quinta aula

--- ByteBank.SistemaInterno/Program.cs
+++ ByteBank.SistemaInterno/Program.cs
@@ -11,12 +11,38 @@ namespace ByteBank.SistemaInterno {
     class Program {
         static void Main (string[] args)
         {
+            List<ContaCorrente> listaContaCorrente = new List<ContaCorrente>();
+
+            ContaCorrente danilo = new ContaCorrente(1, 1006);
+
+            listaContaCorrente.AddAll(
+                danilo,
+                new ContaCorrente(2, 1006),
+                new ContaCorrente(2, 1006),
+                new ContaCorrente(3, 1006),
+                new ContaCorrente(4, 1006),
+                new ContaCorrente(5, 1006),
+                new ContaCorrente(6, 1006)
+            );
+
+            for (int i = 0; i < listaContaCorrente.getSizeArray; i++)
+            {
+                ContaCorrente conta = listaContaCorrente[i];
+                System.Console.WriteLine($"Conta na posição {i} Conta: {conta.Numero} / Agencia: {conta.Agencia}.");
+            }
+
+            Console.WriteLine($"Tamanho do Array: {listaContaCorrente.getIndexSize()}");
+            
+        }
+
+        private static void ListaDeContas()
+        {
             ListaDeContaCorrente listaContaCorrente = new ListaDeContaCorrente();
 
             ContaCorrente danilo = new ContaCorrente(1, 1006);
-            
+
             listaContaCorrente.AddAll(
-                danilo, 
+                danilo,
                 new ContaCorrente(2, 1006),
                 new ContaCorrente(2, 1006),
                 new ContaCorrente(3, 1006),
@@ -34,21 +60,19 @@ namespace ByteBank.SistemaInterno {
             Console.WriteLine($"Tamanho do Array: {listaContaCorrente.getIndexSize()}");
         }
 
-        private static void ApagarContasCorrente(ListaDeContaCorrente listaContaCorrente, ContaCorrente danilo)
-        {
-            listaContaCorrente.PrintContaCorrente();
-            System.Console.WriteLine("Apagar");
-            listaContaCorrente.Remove(danilo);
-            listaContaCorrente.PrintContaCorrente();
+        private static void ApagarContasCorrente (ListaDeContaCorrente listaContaCorrente, ContaCorrente danilo) {
+            listaContaCorrente.PrintContaCorrente ();
+            System.Console.WriteLine ("Apagar");
+            listaContaCorrente.Remove (danilo);
+            listaContaCorrente.PrintContaCorrente ();
         }
 
-        private static void ApgandoConta(){
+        private static void ApgandoConta () {
 
         }
 
-        private static void CriadoArrays()
-        {
-            int[] idades = new int[]{
+        private static void CriadoArrays () {
+            int[] idades = new int[] {
                 1,
                 2,
                 3,
@@ -58,16 +82,14 @@ namespace ByteBank.SistemaInterno {
 
             int somaIdade = 0;
 
-            for (int indice = 0; indice <= idades.Length; indice++)
-            {
-                Console.WriteLine($"Indice na possisão {indice}: idade {idades[indice]} ");
+            for (int indice = 0; indice <= idades.Length; indice++) {
+                Console.WriteLine ($"Indice na possisão {indice}: idade {idades[indice]} ");
                 somaIdade += idades[indice];
             }
-            Console.WriteLine($"A medida de idade é: {somaIdade / idades.Length}");
+            Console.WriteLine ($"A medida de idade é: {somaIdade / idades.Length}");
         }
 
-        private static int[] ArrayDeInt()
-        {
+        private static int[] ArrayDeInt () {
             int[] idades = new int[5];
             idades[0] = 1;
             idades[1] = 2;
@@ -77,30 +99,27 @@ namespace ByteBank.SistemaInterno {
             return idades;
         }
 
-        private static void ManipulacaoDeString(){
-                        string textFirst = "Hey, my number is 967023470.";
+        private static void ManipulacaoDeString () {
+            string textFirst = "Hey, my number is 967023470.";
             string textSecond = "Hellow, the number of smartphone 67023470 is her.";
             string textFinally = "96702-3470, It's number of my smartphone.";
             string padrao = "[0-9]{4,5}-?[0-9]{4}";
 
-            Match matchFirst = Regex.Match(textFirst, padrao);
-            Match matchSecond = Regex.Match(textSecond, padrao);
-            Match matchFinally = Regex.Match(textFinally, padrao);
+            Match matchFirst = Regex.Match (textFirst, padrao);
+            Match matchSecond = Regex.Match (textSecond, padrao);
+            Match matchFinally = Regex.Match (textFinally, padrao);
 
-            Console.WriteLine(String.Format("Firt number {0}, second number {1}, finally number {2}.", matchFirst.Value.ToString(), matchSecond.Value.ToString(), matchFinally.Value.ToString()));
+            Console.WriteLine (String.Format ("Firt number {0}, second number {1}, finally number {2}.", matchFirst.Value.ToString (), matchSecond.Value.ToString (), matchFinally.Value.ToString ()));
         }
 
-        private static void LerArquivo()
-        {
+        private static void LerArquivo () {
             // 1: Escreve um linha para o novo arquivo
-            using (StreamWriter writer = new StreamWriter("C:\\Danilo\\macoratti.txt", true))
-            {
-                writer.WriteLine("Danilo2");
+            using (StreamWriter writer = new StreamWriter ("C:\\Danilo\\macoratti.txt", true)) {
+                writer.WriteLine ("Danilo2");
             }
             // 2: Anexa uma linha ao arquivo
-            using (StreamWriter writer = new StreamWriter(@"C:\\Danilo\macoratti.txt", true))
-            {
-                writer.WriteLine("Beleza4");
+            using (StreamWriter writer = new StreamWriter (@"C:\\Danilo\macoratti.txt", true)) {
+                writer.WriteLine ("Beleza4");
             }
         }
 
